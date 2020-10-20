8523ee69d0e137ddcb26173996ef61d790174ebc
Wanderson Caldas <wcaldasti@gmail.com>
2020-10-20 07:04:14 -0300

    CRIAR PROJETO

---
+++ eSrc/XmlToJson.Core/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace XmlToJson.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string xml = @"<?xml version='1.0' standalone='no'?>
+                        <root>
+                          <person id='1'>
+                          <name>Alan</name>
+                          <url>http://www.google.com</url>
+                          </person>
+                          <person id='2'>
+                          <name>Louis</name>
+                          <url>http://www.yahoo.com</url>
+                          </person>
+                        </root>";
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xml);
+
+            string json = JsonConvert.SerializeXmlNode(doc);
+
+            Console.WriteLine(json);
+            Console.WriteLine("PRESSIONE UMA TECLA PARA SAIR.");
+            Console.ReadKey();
+        }
+    }
+}
